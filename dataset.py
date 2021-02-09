9aa692c00752a9ee930855539e2f3669d5317a05
seungjae-won <ghu9705759@gmail.com>
2021-02-09 22:32:30 +0900

    fix: Fix XDoG function

--- dataset.py
+++ dataset.py
@@ -35,72 +35,28 @@ class Dataset(Dataset):
 
         return self.transformer_appearance(appearance_img), self.transformer_sketch(sketch_img)
 
-def hatch(image):
-  """
-  A naive hatching implementation that takes an image and returns the image in 
-  the style of a drawing created using hatching.
-  image: an n x m single channel matrix.
-  
-  returns: an n x m single channel matrix representing a hatching style image.
-  """
-  xdogImage = xdog(image, 0.1)
+def xdog(image, epsilon=0.5, phi=10, k=1.4, tau=1, sigma=0.5):
+    
+    image = gaussian_filter(image, 0.7)
+    gauss1 = gaussian_filter(image, sigma)
+    gauss2 = gaussian_filter(image, sigma*k)
 
-  hatchTexture = cv2.imread('./textures/hatch.jpg', cv2.CV_LOAD_IMAGE_GRAYSCALE)
+    D = gauss1 - tau*gauss2
 
-  height = len(xdogImage)
-  width = len(xdogImage[0])
+    U = D/255
+    
+    for i in range(0,len(U)):
+        for j in range(0,len(U[0])):
+            U[i][j] = abs(1-U[i][j])
+            
+    for i in range(0, len(U)):
+        for j in range(0, len(U[0])):
+            if U[i][j] >= epsilon:
+                U[i][j] = 1
+            else:
+                ht = np.tanh(phi*(U[i][j] - epsilon))
+                U[i][j] = 1 + ht
 
-  if height > 1080 or width > 1920:
-    print ("This method only supports images up to 1920x1080 pixels in size")
-    sys.exit(1)
-
-  croppedTexture = hatchTexture[0:height, 0:width]
-
-  return xdogImage + croppedTexture
-
-def xdog(image, epsilon=0.01):
-  """
-  Computes the eXtended Difference of Gaussians (XDoG) for a given image. This 
-  is done by taking the regular Difference of Gaussians, thresholding it
-  at some value, and applying the hypertangent function the the unthresholded
-  values.
-  image: an n x m single channel matrix.
-  epsilon: the offset value when computing the hypertangent.
-  returns: an n x m single channel matrix representing the XDoG.
-  """
-  phi = 10
-
-  difference = dog(image, 200, 0.98)/255
-  diff = difference*image
-
-  for i in range(0, len(difference)):
-    for j in range(0, len(difference[0])):
-      if difference[i][j] >= epsilon:
-        difference[i][j] = 1
-      else:
-        ht = np.tanh(phi*(difference[i][j] - epsilon))
-        difference[i][j] = 1 + ht
-
-  return difference*255
-
-def dog(image, k=200, gamma=1):
-  """
-  Computes the Difference of Gaussians (DoG) for a given image. Returns an image 
-  that results from computing the DoG. 
-  image: an n x m array for which the DoG is computed.
-  k: the multiplier the the second Gaussian sigma value.
-  gamma: the multiplier for the second Gaussian result.
-  
-  return: an n x m array representing the DoG
-  """
-
-  s1 = 0.7
-  s2 = s1*k
-
-  gauss1 = gaussian_filter(image, s1)
-  gauss2 = gamma*gaussian_filter(image, s2)
-
-  differenceGauss = gauss1 - gauss2
-  return differenceGauss
+    return U*255
 
 
